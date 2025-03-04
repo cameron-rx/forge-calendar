50da820c7fc8f4f03a7597bfc50501f20829964f
Cameron Robinson <camjr246@yahoo.co.uk>
2025-03-04 15:16:49 +0000

    Implemented crud operations in CalenderItem Repository

---
+++ ForgeAPI/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,11 @@
+public class EntityNotFoundException : Exception
+{
+    public EntityNotFoundException()
+    {
+    }
+
+    public EntityNotFoundException(string message)
+        : base(message)
+    {
+    }
+}
--- ForgeAPI/Repositories/ICalendarItemRepository.cs
+++ ForgeAPI/Repositories/ICalendarItemRepository.cs
@@ -1,7 +1,7 @@
 public interface ICalendarItemRespository
 {
     Task<IEnumerable<CalendarItem>> GetAllCalendarItems(int UserId);
-    Task AddCalendarItem(CalendarItemRequestDTO item);
+    Task AddCalendarItem(CalendarItemRequestDTO item, int UserId);
     Task RemoveCalendarItem(int Id);
     Task UpdateCalendarItem(CalendarItemRequestDTO item, int Id);
 }
