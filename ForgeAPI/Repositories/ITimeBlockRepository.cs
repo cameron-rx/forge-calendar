89c0a44b4f9d8e6f2e6766c74d15a98c8a7c044d
Cameron Robinson <camjr246@yahoo.co.uk>
2025-03-05 11:17:36 +0000

    Removed business logic from repository

--- ForgeAPI/Repositories/ITimeBlockRepository.cs
+++ ForgeAPI/Repositories/ITimeBlockRepository.cs
@@ -1,6 +1,7 @@
 public interface ITimeBlockRespository
 {
     Task<IEnumerable<TimeBlock>> GetAllTimeBlocks(int UserId);
+    Task<TimeBlock?> Get(int timeblockId);
     Task AddTimeBlock(TimeBlock item);
     Task RemoveTimeBlock(int Id);
     Task UpdateTimeBlock(TimeBlock newItem, int oldItemId);
--- ForgeAPI/Repositories/TimeBlockRepository.cs
+++ ForgeAPI/Repositories/TimeBlockRepository.cs
@@ -15,40 +15,31 @@ public class TimeBlockRepository : ITimeBlockRespository
         return await Context.Timeblocks.Where(t => t.UserId == UserId).ToListAsync();
     }
 
+    public async Task<TimeBlock?> Get(int timeblockId)
+    {
+        return await Context.Timeblocks.FindAsync(timeblockId);
+    }
+
     public async Task AddTimeBlock(TimeBlock item)
     {
         Console.WriteLine(item.UserId);
         await Context.Timeblocks.AddAsync(item);
         await Context.SaveChangesAsync();
     }
-    public async Task RemoveTimeBlock(int Id){
-        var item = await Context.Timeblocks.FindAsync(Id);
-        if (item != null)
-        {
-            Context.Timeblocks.Remove(item);
-            await Context.SaveChangesAsync();
-        }
-        else
-        {
-            throw new EntityNotFoundException("Could not find entity associated with given id.");
-        }
+    public async Task RemoveTimeBlock(int timeblockId){
+        var item = await Get(timeblockId);
+        Context.Timeblocks.Remove(item);
+        await Context.SaveChangesAsync();
     }
     public async Task UpdateTimeBlock(TimeBlock newItem, int oldItemId)
     {
-        var oldItem = await Context.Timeblocks.FindAsync(oldItemId);
-        if (oldItem != null)
-        {
-            oldItem.Name = newItem.Name;
-            oldItem.Location = newItem.Location;
-            oldItem.StartTime = newItem.StartTime;
-            oldItem.EndTime = newItem.EndTime;
+        var oldItem = await Get(oldItemId);
+        oldItem.Name = newItem.Name;
+        oldItem.Location = newItem.Location;
+        oldItem.StartTime = newItem.StartTime;
+        oldItem.EndTime = newItem.EndTime;
 
-            await Context.SaveChangesAsync();
-        } 
-        else 
-        {
-            throw new EntityNotFoundException("Could not find enitity associated with given id.");
-        }
+        await Context.SaveChangesAsync();
     }
 }
 
