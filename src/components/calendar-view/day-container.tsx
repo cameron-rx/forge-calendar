983d9d4da7749e81345b104e79f806a09b662f28
Cameron Robinson <camjr246@yahoo.co.uk>
2025-03-14 11:27:36 +0000

    Removed unnecessary call to keys function

--- src/components/calendar-view/day-container.tsx
+++ src/components/calendar-view/day-container.tsx
@@ -1,7 +1,7 @@
 export default function DayContainer() {
     return (
         <>
-            {[...Array(23).keys()].map((_, i) =>
+            {[...Array(23)].map((_, i) =>
                 // Creating dividers for hours in calendar, calculating top position based on index
                 <div className="relative w-full border" style={{ top: (((i + 1) / 24) * 100) + "%" }}></div>
             )}
