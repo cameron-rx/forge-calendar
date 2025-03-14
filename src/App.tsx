6c8ab4fe9746c1a214a9dfbca5a910d785f9a7c3
Cameron Robinson <camjr246@yahoo.co.uk>
2025-03-14 12:28:23 +0000

    Implemented routing for week view

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,31 @@
-import { Separator } from "@radix-ui/react-separator"
-import { AppSidebar } from "./components/side-bar/app-sidebar"
-import { SidebarProvider, SidebarInset, SidebarTrigger } from "./components/ui/sidebar"
-import CalendarContainer from "./components/calendar-view/calendar-container"
-import { useState } from "react"
-import { ButtonArrow } from "./components/ui/button-arrow"
+import { Navigate, replace, Route} from "react-router";
+import { Routes } from "react-router";
+import Week from "./week";
 
 function App() {
-  let [currentDay, setCurrentDay] = useState(new Date(Date.now()))
 
-  function getStartOfWeek() {
-    let dayOfWeek = currentDay.getDay();
-    let startOfWeek = new Date(currentDay);
-    startOfWeek.setDate(currentDay.getDate() - dayOfWeek)
-    return startOfWeek;
+  function weekRedirect() {
+    const today = new Date(Date.now());
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const day = today.getDate() + 1;
+
+    return (
+    <>
+    <Navigate to={`/week/${year}/${month}/${day}`} replace/>
+    </>
+    )
   }
-
-  function getEndOfWeek() {
-    let dayOfWeek = currentDay.getDay();
-    let endOfWeek = new Date(currentDay);
-    endOfWeek.setDate((6-dayOfWeek) + currentDay.getDate())
-    return endOfWeek;
-  }
-
 
   return (
     <>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <header className="sticky top-0 flex z-10 h-16 shrink-0 justify-start items-center gap-2 border-b bg-background px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <div id="WeekSelector" className="flex justify-center align-middle justify-self-center ml-auto mr-auto">
-              <ButtonArrow direction="left"/>
-              <h1 className="text-center m-auto">{getStartOfWeek().toLocaleDateString()} - {getEndOfWeek().toLocaleDateString()}</h1>
-              <ButtonArrow direction="right"/>
-            </div>
-          </header>
-            <CalendarContainer />
-        </SidebarInset>
-      </SidebarProvider>
+    <Routes>
+      <Route path="week">
+        <Route index element={weekRedirect()} />
+        <Route path="*" element={weekRedirect()} />
+        <Route path=":year/:month/:day" element={<Week />} />
+      </Route>
+    </Routes>
     </>
   )
 }
---
+++ src/week.tsx
@@ -0,0 +1,46 @@
+import { AppSidebar } from "./components/side-bar/app-sidebar"
+import { SidebarProvider, SidebarInset, SidebarTrigger } from "./components/ui/sidebar"
+import CalendarContainer from "./components/calendar-view/calendar-container"
+import { useState } from "react"
+import { ButtonArrow } from "./components/ui/button-arrow"
+import { Separator } from "./components/ui/separator"
+
+export default function Week() {
+
+  let [currentDay, setCurrentDay] = useState(new Date(Date.now()))
+
+  function getStartOfWeek() {
+    let dayOfWeek = currentDay.getDay();
+    let startOfWeek = new Date(currentDay);
+    startOfWeek.setDate(currentDay.getDate() - dayOfWeek)
+    return startOfWeek;
+  }
+
+  function getEndOfWeek() {
+    let dayOfWeek = currentDay.getDay();
+    let endOfWeek = new Date(currentDay);
+    endOfWeek.setDate((6-dayOfWeek) + currentDay.getDate())
+    return endOfWeek;
+  }
+
+
+  return (
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="sticky top-0 flex z-10 h-16 shrink-0 justify-start items-center gap-2 border-b bg-background px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <div id="WeekSelector" className="flex justify-center align-middle justify-self-center ml-auto mr-auto">
+              <ButtonArrow direction="left"/>
+              <h1 className="text-center m-auto">{getStartOfWeek().toLocaleDateString()} - {getEndOfWeek().toLocaleDateString()}</h1>
+              <ButtonArrow direction="right"/>
+            </div>
+          </header>
+            <CalendarContainer />
+        </SidebarInset>
+      </SidebarProvider>
+    </>
+  )
+}
