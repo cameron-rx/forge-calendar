07055ce231012c92e423e3e71f426b1b69fdcde4
Cameron Robinson <camjr246@yahoo.co.uk>
2025-03-19 12:35:27 +0000

    Forgot to align based on minutes as well as hours

--- src/components/calendar-view/calendar-block.tsx
+++ src/components/calendar-view/calendar-block.tsx
@@ -6,15 +6,14 @@ interface props {
 
 export default function CalendarBlock({name, startTime, endTime}:props) {
     let startMinutes = startTime.getMinutes()
-    //let startHours = startTime.getHours();
-    let startHours = 3;
+    let startHours = startTime.getHours();
 
     let endMinutes = endTime.getMinutes()
     let endHours = endTime.getHours();
 
 
     return (
-        <div className="z-50 absolute w-full bg-amber-600" style={{ top: ((startHours/24)*100) + "%"}}>
+        <div className="z-50 absolute w-full bg-amber-600" style={{ top: (((startHours/24) + (startMinutes/(24*60)))*100) + "%"}}>
             <h1>Calendar Item</h1>
             <p>{name}</p>
             <p>StartTime: {startTime.getHours() + ":" + startTime.getMinutes()}</p>
