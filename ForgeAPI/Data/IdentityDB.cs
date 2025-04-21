7b3d774f509884b7e80ad1f27c42ed660b95a1a5
Cameron Robinson <camjr246@yahoo.co.uk>
2025-04-21 18:00:21 +0100

    Setup identity service

---
+++ ForgeAPI/Data/IdentityDB.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class IdentityDB : IdentityDbContext<IdentityUser>
+{
+    public IdentityDB(DbContextOptions<IdentityDB> options) : 
+        base(options)
+    { }
+}
